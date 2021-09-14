8bf0e2f40c9d8ec49d99a5bcbfcd841efecd892b
thanhliem-SE <thanhliem2n@gmail.com>
2021-09-14 10:14:34 +0700

    test native branch

--- src/main/java/nguyenthanhliem/demo/springdatajpa/repo/UserRepository.java
+++ src/main/java/nguyenthanhliem/demo/springdatajpa/repo/UserRepository.java
@@ -2,6 +2,12 @@ package nguyenthanhliem.demo.springdatajpa.repo;
 
 import nguyenthanhliem.demo.springdatajpa.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query(value = "select * from user", nativeQuery = true)
+    public List<User> getUsers();
 }
--- src/test/java/nguyenthanhliem/demo/springdatajpa/SpringDataJpaApplicationTests.java
+++ src/test/java/nguyenthanhliem/demo/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,18 @@
 package nguyenthanhliem.demo.springdatajpa;
 
+import nguyenthanhliem.demo.springdatajpa.repo.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private UserRepository userRepository;
 
+    @Test
+    public void getUsersByNativeQuery(){
+        System.out.println("Get Users By Native Query: " + userRepository.getUsers());
+    }
 }
